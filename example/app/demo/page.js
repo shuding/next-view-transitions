531cb661348b30f43d060c78e487ace5c366b1f9
Shu Ding <g@shud.in>
2024-04-15 04:05:54 +0200

    Update page.js

--- example/app/demo/page.js
+++ example/app/demo/page.js
@@ -1,13 +1,18 @@
 import { Link } from 'next-view-transitions'
 
-export default function Page() {
+export default async function Page() {
+  const data = Math.random()
+  await new Promise(r => setTimeout(5000))
+
   return (
     <div className='demo-box'>
       <h2>
         This is the <span className='demo'>Demo</span>
       </h2>
-      <p>OK you just saw the demo :)</p>
+      <p>OK you just saw the demo :) {data}</p>
       <Link href='/'>Go back →</Link>
     </div>
   )
 }
+
+export const dynamic = 'force-dynamic'
