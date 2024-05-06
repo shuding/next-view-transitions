657831b40e7c86478c89f4ef10fd92b6a62c6011
Tsubasa Sakai <javascripter@tsukkun.net>
2024-05-06 22:06:02 +0900

    Fix client-side exception

--- src/browser-native-events.ts
+++ src/browser-native-events.ts
@@ -1,75 +1,68 @@
-import { useEffect, useRef, useSyncExternalStore, use } from 'react'
+import { useEffect, useRef, use, useState } from 'react'
 import { usePathname } from 'next/navigation'
 
 // TODO: This implementation might not be complete when there are nested
 // Suspense boundaries during a route transition. But it should work fine for
 // the most common use cases.
 
-// This is a global variable to keep track of the view transition state.
-let currentViewTransition:
-  | null
-  | [
-      // Promise to wait for the view transition to start
-      Promise<void>,
-      // Resolver to finish the view transition
-      () => void
-    ] = null
-
 export function useBrowserNativeTransitions() {
   const pathname = usePathname()
   const currentPathname = useRef(pathname)
 
-  const transition = useSyncExternalStore(
-    (callback: () => void) => {
-      if (!('startViewTransition' in document)) {
-        return () => {}
-      }
+  // This is a global state to keep track of the view transition state.
+  const [currentViewTransition, setCurrentViewTransition] = useState<
+    | null
+    | [
+        // Promise to wait for the view transition to start
+        Promise<void>,
+        // Resolver to finish the view transition
+        () => void
+      ]
+  >(null)
 
-      const onPopState = () => {
-        let pendingViewTransitionResolve: () => void
+  useEffect(() => {
+    if (!('startViewTransition' in document)) {
+      return () => {}
+    }
 
-        const pendingViewTransition = new Promise<void>((resolve) => {
-          pendingViewTransitionResolve = resolve
+    const onPopState = () => {
+      let pendingViewTransitionResolve: () => void
+
+      const pendingViewTransition = new Promise<void>((resolve) => {
+        pendingViewTransitionResolve = resolve
+      })
+
+      const pendingStartViewTransition = new Promise<void>((resolve) => {
+        // @ts-ignore
+        document.startViewTransition(() => {
+          resolve()
+          return pendingViewTransition
         })
+      })
 
-        const pendingStartViewTransition = new Promise<void>((resolve) => {
-          // @ts-ignore
-          document.startViewTransition(() => {
-            resolve()
-            return pendingViewTransition
-          })
-        })
+      setCurrentViewTransition([
+        pendingStartViewTransition,
+        pendingViewTransitionResolve!,
+      ])
+    }
+    window.addEventListener('popstate', onPopState)
 
-        currentViewTransition = [
-          pendingStartViewTransition,
-          pendingViewTransitionResolve!,
-        ]
+    return () => {
+      window.removeEventListener('popstate', onPopState)
+    }
+  }, [])
 
-        callback()
-      }
-      window.addEventListener('popstate', onPopState)
-
-      return () => {
-        // TODO: Intentionally not cleaning up the event listener, otherwise the
-        // listener won't be registered again. This might be something related
-        // to the `use` call. We should investigate this further.
-      }
-    },
-    () => currentViewTransition,
-    () => null
-  )
-
-  if (transition && currentPathname.current !== pathname) {
+  if (currentViewTransition && currentPathname.current !== pathname) {
     // Whenever the pathname changes, we block the rendering of the new route
     // until the view transition is started (i.e. DOM screenshotted).
-    use(transition[0])
+    use(currentViewTransition[0])
   }
 
   // Keep the transition reference up-to-date.
-  const transitionRef = useRef(transition)
+  const transitionRef = useRef(currentViewTransition)
   useEffect(() => {
-    transitionRef.current = transition
-  }, [transition])
+    transitionRef.current = currentViewTransition
+  }, [currentViewTransition])
 
   useEffect(() => {
     // When the new route component is actually mounted, we finish the view
